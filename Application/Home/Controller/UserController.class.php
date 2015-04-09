d08db67dfdf200726a1e7cb5bf6b3503e89cae34
mutoe <332019319@qq.com>
2015-04-09 22:43:33 +0800

    增加了个人信息页面
    TODO:个人信息页面尚未完成

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -2,9 +2,32 @@
 namespace Home\Controller;
 use Think\Controller;
 class UserController extends BaseController {
-	public function index() {
+
+	public function index($user_id = 0) {
+		if(!$user_id) $user_id = is_login();
+		if(!$user_id) $this -> redirect('Login/index');
+
+		//加载个人分享
+		$share = M('share');
+		$share_list = $share -> where('user_id=%d',$user_id) -> order('create_time desc') -> limit(40) -> select();
+		$this -> assign('share', $share_list);
+
+		//加载个人信息
+		$user = M('user');
+		$data = $user -> where('user_id=%d',$user_id) -> field('password,email', true) -> find();
+		$user_info = M('user_info');
+		$user_info_data = $user_info -> where('user_id=%d',$user_id) -> find();
+		$this -> assign('user', $data);
+		$this -> assign('user_info', $user_info_data);
+
+
 		$this -> display();
 	}
+
+
+	/**
+	 * 登出操作
+	 */
 	public function logout() {
 		cookie('user_id',null);
 		cookie('user_mm',null);
