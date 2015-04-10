3fdf96a229d9fa4d38678ef1f550e87784f975d3
mutoe <332019319@qq.com>
2015-04-10 21:59:43 +0800

    接入Ucenter用户中心

---
+++ Application/Home/Controller/ApiController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ApiController extends \Ucenter\Api\Uc {
+	function index() {
+		$this -> response();
+	}
+}
--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -1,9 +1,12 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
 class TestController extends Controller {
 
-	public function index($share_id = 0) {
-		$this -> error('未知原因，如有需要请求助管理员:','',200);
+	public function index() {
+		$uc = new \Ucenter\Client\Client();
+		$result = $uc -> uc_user_register('mttttt' , '22222' , '332019319@qq.com');
+		dump($result) ;
 	}
 }
