f0aca9f692fcfa137d699ec84cbad3d6bd0635c5
mutoe <332019319@qq.com>
2015-04-15 21:56:18 +0800

    [0.1.6.150415a]

--- Application/Home/Controller/TagController.class.php
+++ Application/Home/Controller/TagController.class.php
@@ -4,17 +4,12 @@ use Think\Controller;
 class TagController extends BaseController {
 	public function index() {
 		$tag = M('tag');
-		$tag_list = $tag -> order('total_share desc,create_time desc') -> limit(0,16) -> select();
+		$tag_list = $tag -> order('total_share desc,create_time desc') -> limit(40) -> select();
 		$this -> assign('tag', $tag_list);
 
 		$share = M('share');
-		foreach ($tag_list as $key => $tag) {
-			$map['tag_id'] = array('like', '%'.$tag['tag_id'].'%');
-			$share_list[$key] = $share -> where($map) -> order('create_time desc') -> limit(0,8) -> select();
-		}
+		$share_list = $share -> order('create_time desc') -> limit(100) -> select();
 		$this -> assign('share', $share_list);
-		//$this -> assign('test', $share_list);
-
 
 		$this -> display();
 	}
