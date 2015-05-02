f6eab696fdd17fe765ba707ef494a947aeb45317
mutoe <332019319@qq.com>
2015-05-02 16:28:25 +0800

    uc通信 @freyo

--- Application/Ucenter/Client/uc_client/data/cache/apps.php
+++ Application/Ucenter/Client/uc_client/data/cache/apps.php
@@ -3,23 +3,169 @@ $_CACHE['apps'] = array (
   1 => 
   array (
     'appid' => '1',
-    'type' => 'OTHER',
-    'name' => 'yiika_custom',
-    'url' => 'http://localhost/yiika/Custom',
+    'type' => 'DISCUZ',
+    'name' => '天佑年华BBS',
+    'url' => 'http://bbs.cqjtu.edu.cn',
     'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  4 => 
+  array (
+    'appid' => '4',
+    'type' => 'UCHOME',
+    'name' => '天佑家园',
+    'url' => 'http://bbs.cqjtu.edu.cn/home',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'gbk',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  5 => 
+  array (
+    'appid' => '5',
+    'type' => 'OTHER',
+    'name' => '天佑星空',
+    'url' => 'http://bbs.cqjtu.edu.cn/tysky',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  12 => 
+  array (
+    'appid' => '12',
+    'type' => 'ECMALL',
+    'name' => '天佑年华网络商城',
+    'url' => 'http://bbs.cqjtu.edu.cn/eshop',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'gbk',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  13 => 
+  array (
+    'appid' => '13',
+    'type' => 'OTHER',
+    'name' => '天佑年华音乐站',
+    'url' => 'http://bbs.cqjtu.edu.cn/music',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  14 => 
+  array (
+    'appid' => '14',
+    'type' => 'SUPESITE',
+    'name' => '天佑年华社区门户_SupeSite',
+    'url' => 'http://bbs.cqjtu.edu.cn/cms',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'gbk',
+    'synlogin' => '1',
+    'extra' => '',
+    'recvnote' => '1',
+  ),
+  15 => 
+  array (
+    'appid' => '15',
+    'type' => 'OTHER',
+    'name' => '天佑网盘',
+    'url' => 'http://disk.cqjtu.edu.cn',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  16 => 
+  array (
+    'appid' => '16',
+    'type' => 'OTHER',
+    'name' => '天佑学长',
+    'url' => 'http://xz.cqjtu.edu.cn',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  18 => 
+  array (
+    'appid' => '18',
+    'type' => 'OTHER',
+    'name' => '天佑文库',
+    'url' => 'http://10.1.104.220/api',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  19 => 
+  array (
+    'appid' => '19',
+    'type' => 'OTHER',
+    'name' => '天佑拼图',
+    'url' => 'http://202.202.240.89/timeline/index.php/home',
+    'ip' => '202.202.240.89',
     'viewprourl' => '',
     'apifilename' => 'index',
     'charset' => '',
-    'dbcharset' => '',
     'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
     'recvnote' => '1',
-    'extra' => false,
-    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
-<root>
-	<item id="template"><![CDATA[]]></item>
-</root>',
-    'allowips' => '',
   ),
+  'UC_API' => 'http://bbs.cqjtu.edu.cn/uc_server',
 );
-
-?>
