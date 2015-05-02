61fdb3af51b6f5f61d0d6e886efff5dc424ed45d
mutoe <332019319@qq.com>
2015-05-02 17:46:21 +0800

    修复了管理用户组入口错误

--- Application/Admin/Controller/MemberController.class.php
+++ Application/Admin/Controller/MemberController.class.php
@@ -6,8 +6,8 @@ class MemberController extends BaseController {
 	public function user() {
 		$user = M('user');
 		$data = $user -> select();
-
 		$this -> assign('data',$data);
+
 		$this -> display();
 	}
 
@@ -15,13 +15,16 @@ class MemberController extends BaseController {
 		$user = M('user');
 		$map['group_id'] = array('IN','1,2');
 		$data = $user -> where($map) -> select();
-
 		$this -> assign('data',$data);
+
 		$this -> display();
 	}
 
-	public function mod_user($user_id = 0) {
-		if(!$user_id) $this -> error('参数操作');
+	/**
+	 * 更改用户组
+	 */
+	public function modUser($user_id = 0) {
+		if(!$user_id) $this -> error('参数有误');
 		$user_group = M('user_group');
 		$group = $user_group -> select();
 		$this -> assign('group', $group);
@@ -32,4 +35,25 @@ class MemberController extends BaseController {
 
 		$this -> display();
 	}
+
+	/**
+	 * 删除用户操作
+	 */
+	public function delUser($user_id = 0) {
+		if(!$user_id) $this -> error('参数有误');
+		$flag = 1;
+		$user = M('user');
+		$result = $user -> delete($user_id);
+		if(!$result) $flag = 0;
+		$user_info = M('user_info');
+		$result = $user_info -> delete($user_id);
+		if(!$result) $flag = 0;
+
+		if($flag) {
+			$this -> success();
+		} else {
+			$this -> error('删除失败！');
+		}
+	}
+
 }
