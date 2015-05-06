4c5437a4fb68ce45ccf96b80b3499e2411e2ac5f
mutoe <332019319@qq.com>
2015-05-06 23:57:05 +0800

    rewrite tag handle function
    ### fixed ###
    * 重写了修改分享时的tag处理算法

--- Application/Home/Model/ShareModel.class.php
+++ Application/Home/Model/ShareModel.class.php
@@ -15,60 +15,50 @@ class ShareModel extends Model {
 
 	/**
 	 * 将文字字符串标签(用' '分割)替换成tag_id字符串(用,分隔) - 自动完成方法
-	 * @param string 文字字符串标签(用' '分割)
-	 * @return string id标签字符串(用,分隔)
+	 * @param json json串形式的tag列表
+	 * @return string id标签json串
 	 * @author mt
 	 */
-	protected function tag_fun($tag_org) {
-		if($tag_org == '') return null;
+	protected function tag_fun($tag_json) {
+		$data = explode(',', $tag_json);
+
+		// 去除首尾空格
+		foreach ($data as $value) $value = trim($value, ' ');
+		// 去重
+		$data = array_unique($data);
+
 		$tag = M('tag');
-		$tag_org = trim($tag_org, ' ');
-		$tag_arr = explode(',', $tag_org);
-		$result = '';
-		foreach ($tag_arr as $value) {
-			$value = trim($value, ' ');
-			if($value == '') continue;
+		$user_id = is_login();
+		$result = array();
+		foreach ($data as $value) {
 			$tgid = $tag -> where("tag_name='%s'",$value) -> getField('tag_id');
 			if($tgid) {
 				//如果存在该标签
-				$result .= $tgid.',';
-				$tag -> where("tag_name='%s'",$value) -> setInc('total_share');
+				array_push($result, $tgid);
+				$tag -> where("tag_id=%d", $tgid) -> setInc('total_share');	// 标签分享数自增
 			} else {
 				//新建标签
 				$data['tag_name'] = $value;
-				$data['create_user'] = is_login();
+				$data['create_user'] = $user_id;
 				$data['create_time'] = time();
 				$data['total_share'] = 1; //新增一个分享
-				$add_tag = $tag -> add($data);
-				$result .= $add_tag.',';
+				$tgid = $tag -> add($data);
+				array_push($result, $tgid);
 			}
 		}
-		$result = rtrim($result, ',');	// 去除尾部分隔符
-		return str2json( $result );
+		return json_encode( $result );
 	}
 
 	/**
 	 * 验证标签格式方法 - 字段自动验证方法
-	 * @param string 原始文字标签字符串
+	 * @param json json串形式的tag列表
 	 * @author mt
 	 */
-	protected function check_tag($tag_org) {
-		// 如果没有输入，则通过验证
-		if($tag_org == '') return true;
-		// 如果去除所有分隔符号的结果为空，则验证失败。如“ , ,,”
-		$match = preg_replace("/[\s,]+/",'', $tag_org);
-		if($match == '') return false;
-		//去除首尾空格
-		$tag_org = trim($tag_org, ' ');
-		//匹配中文逗号作为分隔符
-		$tag_arr = explode(",", $tag_org);
-		//当超过 7 个标签不通过验证
-		if(count($tag_arr) > 7) return false;
-		$result = '';
-		//每个标签超过 18 个字符不通过匹配(6个汉字)
-		foreach ($tag_arr as $value) {
-			$value = trim($value, ' ');
-			if(strlen($value) > 18) return false;
+	protected function check_tag($tag_json) {
+		$data = json_decode($tag_json);
+		if( count($data) > 7 ) return false;
+		foreach ($data as $value) {
+			if( mb_strlen($value, 'utf8') > 12 ) return false;
 		}
 		return true;
 	}
