d6429d0b77f10c1ea1171d12fe837ff6923df00c
mutoe <332019319@qq.com>
2015-05-09 17:42:47 +0800

    [0.1.17.150508b] add avgrund model plugin
    增加了全站 ajax 无跳转登陆登出功能

--- Public/js/app.js
+++ Public/js/app.js
@@ -15,6 +15,40 @@
 	});
 
 
+	$('#model-login').avgrund({
+		height: 340,
+		closeByDocument: false, // ..and by clicking document itself
+		holderClass: 'model-login',
+		onBlurContainer: '.container',
+		template: '<form class="am-form" data-am-validator>'+
+  	'<fieldset>'+
+  		'<legend><div class="logo"></div> 登陆</legend>'+
+	    '<label for="model-nickname">用户名:</label>'+
+	    '<input type="text" id="model-nickname" name="nickname" minlength="2" maxlength="24">'+
+	    '<br />'+
+	    '<label for="model-passwd">密码:</label>'+
+	    '<input type="password" id="model-passwd" name="password" minlength="3" maxlength="32">'+
+	    '<br />'+
+	    '<div class="am-cf">'+
+	      '<input type="button" value="登录" class="am-btn am-btn-primary am-fr" onclick="modelLogin()">'+
+	      '<a class="am-btn am-btn-default am-fl" href="http://bbs.cqjtu.edu.cn/register.php">没有帐号 ^_^?</a>'+
+	    '</div>'+
+    '</fieldset>'+
+  '</form>'
+	});
+
+	$('#model-popup').avgrund({
+		height: 200,
+		width: 480,
+		holderClass: 'model-popup',
+		onBlurContainer: '.container',
+		template: '<div class="header">'+
+				'<h2 class="am-text-success am-text-bottom"><i class="am-icon-smile-o am-icon-md"></i> <span class="title">成功了</span> <span class="am-text-sm">天佑时光轴</span></h2>'+
+			'</div>'+
+			'<hr />'+
+			'<div class="info">欢迎回来 上士 木头没有强迫症</div>'
+	});
+
 })(jQuery);
 
 
@@ -45,4 +79,75 @@ var like = function (el) {
   		alert(data.info);
   	}
   });
+}
+
+/**
+ * 模态登陆
+ */
+var modelLogin = function() {
+	$(".avgrund-close").click();
+	$.ajax({
+		type:"post",
+		url: ROOT + "/Login/index.html",
+		data: {
+			'nickname': $('#model-nickname').val(),
+			'password': $("#model-passwd").val()
+		},
+		success: function(data) {
+			if (data.status) {
+				$(".avgrund-close").click();
+				setTimeout(function() {
+					popup('登陆成功,正在返回...');
+				}, 501);
+				setTimeout(function() {
+					location.reload(true);
+				}, 3000);
+			} else {
+				popup('登陆失败:' + data.info, false);
+			}
+		}
+	});
+}
+
+var modelLogout = function() {
+	if ( confirm("您确实要登出吗？") ) {
+		$.ajax({
+			url: ROOT + "/Base/logout.html",
+			success: function(data) {
+				if (data.status) {
+					setTimeout(function() {
+						popup('登出成功,正在返回首页...');
+					}, 501);
+					setTimeout(function() {
+						location.href = ROOT;
+					}, 1000);
+				} else {
+					popup('登陆失败:' + data.info, false);
+				}
+			}
+		});
+	}
+}
+
+var modelSign = function() {
+	popup(
+		'本站已接入校内论坛，请<a href="http://bbs.cqjtu.edu.cn/register.php">移步论坛</a>进行注册，或<a href="'+ ROOT +'/Login/index.html">点此使用论坛帐号进行登陆</a>',
+		true, '请使用论坛帐号进行登陆'
+	);
+}
+
+/**
+ * popup 提示窗口
+ * @param {String} info
+ * @param {Boolean} success
+ */
+var popup = function(info, success = true, title = null) {
+	$("#model-popup").click();
+	if(!success) {
+		$(".model-popup .header h2").removeClass('am-text-success').addClass('am-text-error');
+		$(".model-popup .header h2 i").removeClass('am-icon-smile-o').addClass('am-icon-times-circle');
+		$(".model-popup .header h2 .title").html('出错了');
+	}
+	if(title != null) $(".model-popup .header h2 .title").html( title );
+	$(".model-popup .info").html( info );
 }
